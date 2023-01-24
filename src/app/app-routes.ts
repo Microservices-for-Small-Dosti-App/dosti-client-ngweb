ed654ffc889e7b09a28418db40b4e9e88e3b8fcd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-24 09:59:38 +0530

    Code Refactoring

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 
 export const APP_ROUTES: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadComponent: () => import('./modules/home/home.component').then((m) => m.HomeComponent) },
-  { path: 'errors', loadChildren: () => import('./core/errors/errors.module').then(m => m.ErrorsModule) },
+  { path: 'home', loadComponent: () => import('./modules/home/home.component').then((c) => c.HomeComponent) },
+  { path: 'errors/not-found', loadComponent: () => import('./core/errors/not-found/not-found.component').then(c => c.NotFoundComponent) },
   { path: '**', redirectTo: 'errors/not-found', pathMatch: 'full' },
 ];
 
