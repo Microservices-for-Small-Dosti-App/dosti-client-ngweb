0aae18692f26ef5061b58d3d6935a185136b2272
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-24 09:34:35 +0530

    Code Refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,26 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { PreloadAllModules, provideRouter, withDebugTracing, withPreloading, } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_ROUTES } from './app/app-routes';
 
 const moduleoid = {
-  // providers: [
-  //   importProvidersFrom(HttpClientModule),
-  //   provideRouter(
-  //     APP_ROUTES,
-  //     withPreloading(PreloadAllModules),
-  //     withDebugTracing()
-  //   ),
-  // ],
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideRouter(
+      APP_ROUTES,
+      withPreloading(PreloadAllModules),
+      withDebugTracing()
+    ),
+  ],
 };
 
 const environment = { production: false };
 if (environment.production) enableProdMode();
 
-bootstrapApplication(AppComponent).catch(console.error);
+bootstrapApplication(AppComponent, moduleoid).catch(console.error);
 
 // import { AppModule } from './app/app.module';
 
