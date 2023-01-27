7f41ebf7b91db8b55dafead3c6c44da83ea31819
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-27 12:01:35 +0530

    Display Products

--- src/app/modules/products/products-list/products-list.component.ts
+++ src/app/modules/products/products-list/products-list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsService } from '../products.service';
 import { MatPaginatorModule } from '@angular/material/paginator'
 import { ProductQuickviewComponent } from '../product-quickview/product-quickview.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-products-list',
@@ -11,14 +12,19 @@ import { ProductQuickviewComponent } from '../product-quickview/product-quickvie
   imports: [
     CommonModule,
     MatPaginatorModule,
-    ProductQuickviewComponent
+    ProductQuickviewComponent,
+    HttpClientModule
   ],
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
 
   constructor(public productsService: ProductsService) {
+  }
+
+  ngOnInit(): void {
+    this.productsService.getProducts();
   }
 
 }
