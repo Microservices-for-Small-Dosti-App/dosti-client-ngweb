d63d317f8a8ab7943e715a738ff69fe501b2d65f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-27 11:31:55 +0530

    Product Quick View

--- src/app/modules/products/product-quickview/product-quickview.component.ts
+++ src/app/modules/products/product-quickview/product-quickview.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { IProductQuickView } from './iproduct-quickview';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-product-quickview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    NgOptimizedImage,
+  ],
   templateUrl: './product-quickview.component.html',
   styleUrls: ['./product-quickview.component.scss']
 })
 export class ProductQuickviewComponent {
 
+  @Input()
+  product: IProductQuickView = {} as IProductQuickView;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.product);
+  }
 }
