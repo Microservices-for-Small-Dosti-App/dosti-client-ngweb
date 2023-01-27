162f08c4f30402904a75c81d245f488b79bafdb8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-27 20:22:19 +0530

    Code Refactoring

--- src/app/modules/products/product-quickview/iproduct-quickview.ts
+++ src/app/modules/products/product-quickview/iproduct-quickview.ts
@@ -1,10 +1,14 @@
-export interface IProductQuickView {
-    title: string;
+import { IQuickview } from "~/app/core/Interfaces/iquickview";
+import { Common } from "~/app/core/utilities/type.utilities";
+import { IProduct } from "../iproduct";
 
+export type IProductQuickView = Common<IProduct, IQuickview> & {
     price: number;
-
-    thumbnail: string;
 }
 
+// type keys = keyof IProductQuickView;
+// type Common<T, U> = Record<Extract<keyof T, keyof U>, U>;
 // type IProductQuickView = Omit<IProduct, "name" | "age">;
 // export type IProductQuickView = Omit<IProduct, "dummyData">;
+// export interface IProductQuickView {
+// }
--- src/app/modules/products/products.utilities.ts
+++ src/app/modules/products/products.utilities.ts
@@ -3,7 +3,7 @@ import type { IProductQuickView } from "./product-quickview/iproduct-quickview";
 
 // This is Functional Programming. We can also move this into products.service.ts.
 export function getQuickViewsFromProducts(products: IProduct[]): IProductQuickView[] {
-    return products.map((product: IProductQuickView) => {
+    return products.map((product: any) => {
         return {
             title: product.title,
             price: product.price,
