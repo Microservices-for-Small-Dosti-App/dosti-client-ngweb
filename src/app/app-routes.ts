b712bfbd80fe8d0f59a53edece09af7be5d45046
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-18 18:26:53 +0530

    Code Refactoring

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -16,7 +16,7 @@ export const APP_ROUTES: Routes = [
   },
   {
     path: 'errors/not-found', loadComponent:
-      () => import('./core/errors/not-found/not-found.component').then(c => c.NotFoundComponent)
+      () => import('./modules/shared/errors/not-found/not-found.component').then(c => c.NotFoundComponent)
   },
   { path: '**', redirectTo: 'errors/not-found', pathMatch: 'full' },
 ];
